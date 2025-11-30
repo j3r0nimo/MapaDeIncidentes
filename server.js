0da9dac3efb354373e936337ec0f8576578364e6
j3r0nimo <jerobalort@gmail.com>
2025-11-30 14:39:33 -0300

    instalacion cors
    elimino la configuracion de json server y agrego archivo server.js para conexion de render

---
+++ server.js
@@ -0,0 +1,26 @@
+const jsonServer = require('json-server');
+const cors = require('cors');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults({ 
+    static: false 
+});
+
+const corsOptions = {
+  origin: ["https://mapadeincidentesfront.onrender.com/", "http://localhost:5173"], 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+server.use(cors(corsOptions));
+
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 3000; 
+
+server.listen(port, () => {
+  console.log(`JSON Server está corriendo en el puerto ${port}`);
+});
