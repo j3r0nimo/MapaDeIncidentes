176d566d94bd117d2f4924a20f06040b01d042db
j3r0nimo <jerobalort@gmail.com>
2025-11-30 14:44:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const jsonServer = require('json-server');
-const cors = require('cors');
+import jsonServer from 'json-server';
+import cors from 'cors';
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({ 
-    static: false 
+const middlewares = jsonServer.defaults({
+    static: false
 });
 
 const corsOptions = {
-  origin: ["https://mapadeincidentesfront.onrender.com/", "http://localhost:5173"], 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204
+    origin: ["https://mapadeincidentesfront.onrender.com/", "http://localhost:5173"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
 };
 
 server.use(cors(corsOptions));
@@ -19,8 +19,8 @@ server.use(cors(corsOptions));
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log(`JSON Server está corriendo en el puerto ${port}`);
+    console.log(`JSON Server está corriendo en el puerto ${port}`);
 });
